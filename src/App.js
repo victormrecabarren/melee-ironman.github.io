9d60314ac009f4a3c436a675d160f5f21f5cba54
Victor Recabarren <victormrecabarren@gmail.com>
2019-08-03 14:02:12 -0400

    setting up pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,162 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import Table from './components/Table'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+
+class App extends Component {
+  state = {
+    characters: [],
+    secondColumn: [],
+    playerOne: [],
+    playerTwo: [],
+    playerOneRemaining: [],
+    playerTwoRemaining: []
+  }
+
+  componentDidMount() {
+    this.getCharacters()
+
+  }
+
+  getCharacters = () => {
+    fetch('/characters')
+    // .then(res => console.log(res))
+    .then(response => response.json())
+    .then(json => {
+      let copy = [...json].reverse()
+      this.setState({
+      characters: json,
+      secondColumn: copy,
+      playerOne: json,
+      playerTwo: copy,
+      playerOneRemaining: json,
+      playerTwoRemaining: copy
+    })
+  })
+    .catch(error => console.log(error))
+  }
+
+  randomize = () => {
+    let charactersCopy = [...this.state.characters]
+
+    charactersCopy.sort(() => {
+      return .5 - Math.random()
+    })
+    let randomized =     [...this.state.characters].sort(() => {
+          return .5 - Math.random()
+        })
+      this.setState({
+        characters: charactersCopy,
+        secondColumn: randomized,
+        playerOne: charactersCopy,
+        secondColumn: randomized,
+        playerOneRemaining: charactersCopy,
+        playerTwoRemaining: randomized
+      })
+  }
+
+  reorder = () => {
+    let ordered = [...this.state.characters]
+
+    ordered.sort((a,b) => {
+        return a.trueIndex - b.trueIndex
+    })
+
+    let reverseOrder = [...ordered].reverse()
+
+    this.setState({
+      characters: ordered,
+      secondColumn: reverseOrder,
+      playerOne: ordered,
+      playerTwo: reverseOrder,
+      playerOneRemaining: ordered,
+      playerTwoRemaining: reverseOrder
+    })
+  }
+
+  dittos = () => {
+    this.setState({
+      secondColumn: this.state.characters,
+      playerOne: this.state.characters,
+      playerTwo: this.state.characters,
+      playerOneRemaining: this.state.characters,
+      playerTwoRemaining: this.state.characters,
+    })
+  }
+
+  handleKill = (char, winner, loser) => {
+
+    let losersRemaining = [...this.state[`${loser}Remaining`]]
+
+    let winnersRemaining = [...this.state[`${winner}Remaining`]]
+
+    let winnersStats = [...this.state[winner]]
+
+    let losersStats = [...this.state[loser]]
+
+    let winningIndex = winnersStats.findIndex((char) => char.id == winnersRemaining[0].id)
+
+    let losingIndex = losersStats.findIndex((char) => char.id == losersRemaining[0].id)
+
+
+    winnersRemaining[0].beat
+    ? winnersStats[winningIndex].beat.push({
+      name: char.name,
+      icon: char.icon
+    })
+    : winnersStats[winningIndex].beat = [{
+      name: char.name,
+      icon: char.icon
+    }]
+
+    winnersRemaining[0].totalWins
+    ? winnersStats[winningIndex].totalWins += 1
+    : winnersStats[winningIndex].totalWins = 1
+
+
+    losersRemaining[0].losses
+    ? losersStats[losingIndex].losses.push({
+      name: winnersRemaining[0].name, icon: winnersRemaining[0].icon
+    })
+    : losersStats[losingIndex].losses = [{
+      name: winnersRemaining[0].name,
+      icon: winnersRemaining[0].icon
+    }]
+
+    if (losersStats[losingIndex].losses.length === 2){
+      losersRemaining.shift()
+    }
+
+
+    this.setState({
+      [winner]: winnersStats,
+      [loser]: losersStats,
+      [`${winner}Remaining`]: winnersRemaining,
+      [`${loser}Remaining`]: losersRemaining
+    })
+
+
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <Table
+          characters={this.state.characters}
+          secondColumn={this.state.secondColumn}
+          randomize = {this.randomize}
+          reorder = {this.reorder}
+          dittos = {this.dittos}
+          handleKill = {this.handleKill}
+
+        />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ src/components/Table.js
@@ -0,0 +1,155 @@
+import React, { Component } from 'react'
+
+function importAll(r) {
+  let images = {}
+  r.keys().forEach((item, index) => { images[item.replace('./', '')] = r(item); })
+  return images
+}
+
+const images = importAll(require.context('../icons', false, /\.(png|jpe?g|svg)$/))
+
+let winnerObject = {}
+for (let i=1; i<=26; i++) {
+  winnerObject[i] = ''
+}
+
+
+class Table extends Component {
+  state = {
+    playerOneName: 'Player 1',
+    playerTwoName: 'Player 2',
+    winner: winnerObject,
+    playerOne: [],
+    playerTwo: [],
+  }
+
+  lostStock(event, char, winner, loser) {
+
+    //dim or undim the icon
+    if (event.target.classList.contains('clicked')) {
+      event.target.classList.remove('clicked')
+    } else {
+      event.target.classList.add('clicked')
+      this.props.handleKill(char, winner, loser)
+    }
+
+    //
+    console.log(winner, "wins");
+
+    let playerWins = [...this.state[winner]]
+    playerWins.push(char)
+
+    this.setState({
+      [winner]: playerWins
+    })
+
+
+
+  }
+
+
+
+  render() {
+
+    return(
+
+      <>
+      <button className='btn btn-warning'
+        onClick={() => {
+          this.props.dittos()
+        }}>Dittos</button>
+      <table className='table table-hover'>
+        <thead>
+          <tr className='sticky-top'>
+            <th
+              onClick={() => {
+                this.props.randomize()
+              }}
+              scope="col"
+              className='sticky-top'
+              ><button className="btn btn-success">Randomize!</button></th>
+            <th
+              scope="col"
+              className='sticky-top'
+              >{this.state.playerOneName}</th>
+            <th
+              scope="col"
+              className='sticky-top'
+              >{this.state.playerTwoName}</th>
+            <th
+              scope="col"
+              className='sticky-top'
+              ><button
+                className='btn btn-danger'
+                onClick={() => {
+                  this.props.reorder()
+                }}
+                >Reorder</button></th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            this.props.characters.map((char, i) => { return(
+              <tr key={char.id}>
+              <th scope='row'>{i+1}</th>
+              <td>
+                <img
+                  className='icon'
+                  src={images[char.icon]}
+                  alt={char.name}
+                  onClick={(event) => {
+                    this.lostStock(event, char, 'playerTwo', 'playerOne')
+                  }}
+                />
+                <img
+                  className='icon'
+                  src={images[char.icon]}
+                  alt={char.name}
+                  onClick={(event) => {
+                    this.lostStock(event, char, 'playerTwo', 'playerOne')
+                  }}
+                />
+              </td>
+              <td>
+                <img
+                  className='icon'
+                  src={images[this.props.secondColumn[i].icon]}
+                  alt={this.props.secondColumn[i].name}
+                  onClick={(event) => {
+                    this.lostStock(event, this.props.secondColumn[i], 'playerOne', 'playerTwo')
+                  }}
+                />
+                <img
+                  className='icon'
+                  src={images[this.props.secondColumn[i].icon]}
+                  alt={this.props.secondColumn[i].name}
+                  onClick={(event) => {
+                    this.lostStock(event, this.props.secondColumn[i], 'playerOne', 'playerTwo')
+                  }}
+                />
+              </td>
+              <td>
+                {
+                  this.state.winner[i]
+                    ? <img
+                      src={images[this.state.winner[i]]}
+                      alt={char.name}
+                      />
+                    : null
+                }
+              </td>
+            </tr>
+            )})
+          }
+
+        </tbody>
+      </table>
+
+
+
+    </>
+    )
+  }
+}
+
+export default Table
