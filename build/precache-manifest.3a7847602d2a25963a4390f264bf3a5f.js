57dcc57f017f706c1ea3532975307a8c8c48b3b4
Victor Recabarren <victormrecabarren@gmail.com>
2019-08-03 15:52:24 -0400

    removed build from gitignore

---
+++ build/precache-manifest.3a7847602d2a25963a4390f264bf3a5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5036ab115d696ea4ef7b6aa2b310fd88",
+    "url": "/melee-ironman/index.html"
+  },
+  {
+    "revision": "2abeb7d0da43f2dc6eb8",
+    "url": "/melee-ironman/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "b07c6dd369c812ca1b6f",
+    "url": "/melee-ironman/static/css/main.4cf40208.chunk.css"
+  },
+  {
+    "revision": "2abeb7d0da43f2dc6eb8",
+    "url": "/melee-ironman/static/js/2.c6d22b0a.chunk.js"
+  },
+  {
+    "revision": "b07c6dd369c812ca1b6f",
+    "url": "/melee-ironman/static/js/main.c8ebc13b.chunk.js"
+  },
+  {
+    "revision": "82aa760228696dcc58e0",
+    "url": "/melee-ironman/static/js/runtime~main.9fe196d4.js"
+  }
+]);
